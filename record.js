81b9ee1df8299c0be60b5c6997b6847e411e6a3d
atherion-wq <67752809+atherion-wq@users.noreply.github.com>
2022-10-27 16:38:00 -0500

    Update record.js
    Se agregan los records para obtener preguntas, crear pregunta, responder pregunta y conseguir promociones

--- record.js
+++ record.js
@@ -25,7 +25,6 @@ recordRoutes.get('/records', (req, res) =>{
   
 });
 
-
 recordRoutes.get('/get/Usuarios', (req, res) =>{
   dbo.connection.useDb('MariajuanaDb').collection("Usuario").find({})
   .toArray(function (err, result) {
@@ -34,7 +33,6 @@ recordRoutes.get('/get/Usuarios', (req, res) =>{
   });
   
 });
-
 
 recordRoutes.get('/productos', (req, res) =>{
   dbo.connection.useDb('MariajuanaDb').collection("Producto").find({})
@@ -63,5 +61,46 @@ recordRoutes.post('/add/usuario', (req, res) =>{
       });
 });
  
+recordRoutes.get('/preguntas', (req, res) =>{
+  dbo.connection.useDb('MariajuanaDb').collection("Pregunta").aggregate(
+    [{$lookup:{from :"Usuario",localField:"usuario",foreignField:"_id", as: "usuario"}}])
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
+recordRoutes.post('/add/pregunta', (req, res) =>{
+  var userid = req.body.usuario;
+  var o_id = new ObjectId(userid);  
+  let myobj = {
+      contenido:req.body.contenido,        
+      usuario: o_id,
+      respuesta:  req.body.respuesta
+    };
+    
+  dbo.connection.useDb('MariajuanaDb').collection("Pregunta").insertOne(myobj, function (err, result) {
+      if (err) console.log (err);
+      res.json(result);
+    });
+});
+
+recordRoutes.post('/question', (req, res) =>{  
+  dbo.connection.useDb('MariajuanaDb').collection("Pregunta")
+  .updateOne({_id: ObjectId(req.body.preguntaId)},{$set:{respuesta:req.body.respuesta}}, function(err,result){
+    if (err) console.log (err);
+    res.json(result);
+  })
+});
+
+recordRoutes.get('/promociones', (req, res) =>{
+  dbo.connection.useDb('MariajuanaDb').collection("Promocion").aggregate(
+    [{$lookup:{from :"Producto",localField:"producto",foreignField:"_id", as: "producto"}}])
+    .toArray(function (err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+});
+
 
 module.exports = recordRoutes;
